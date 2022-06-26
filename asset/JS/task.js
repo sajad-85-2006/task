fbae13291e759472d8a895deabf7013af7d01a5b
sajad-85-2006 <104766821+sajad-85-2006@users.noreply.github.com>
2022-06-26 21:46:00 +0430

    add project

---
+++ asset/JS/task.js
@@ -0,0 +1,18 @@
+new Vue({
+    
+    el: '#app',
+    data() {
+
+        return {
+            product: 'helow',
+            modal: false,
+            modal_1: false,
+            modal_2: false,
+            edit: false,
+            info: null,
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:9000/task').then(response => (this.info = response))
+    }
+})
